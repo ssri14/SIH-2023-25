7c8f82c705e0e407a2482edcacaea82391d49b6d
AnuragRanjan2003 <anuragranjan1261@gmail.com>
2023-09-26 23:52:17 +0530

    sadasd

---
+++ lib/features/main/controller/map_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapController extends GetxController{
+
+    Rxn<GoogleMap> map = Rxn();
+
+
+
+}
