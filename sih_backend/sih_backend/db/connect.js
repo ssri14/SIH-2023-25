7b8b6f5302b884f80c4996587521da8a481098a3
Kaushan Dey <112932397+KaushanDey@users.noreply.github.com>
2023-09-27 13:34:56 +0530

    Delete sih_backend/sih_backend directory

--- sih_backend/sih_backend/db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require("mongoose");
-
-const connectDB = (uri)=>{
-    return mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-};
-module.exports=connectDB;
